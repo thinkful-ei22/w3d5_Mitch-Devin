a16b54f1da070d8ca695b0034627043bdc8536f5
MitchGianoni <mitchgianoni@yahoo.com>
2018-06-29 11:55:48 -0400

    initial commit

---
+++ node-intro-to-unit-testing/adder.js
@@ -0,0 +1,10 @@
+'use strict';
+function adder(a, b) {
+  // validate that both inputs are numbers
+  if (!(typeof a === 'number' && typeof b === 'number')) {
+    throw Error('Oh no!');
+  }
+  return a + b;
+}
+
+module.exports = adder;
